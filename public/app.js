097cc6b4e1f7190514cc2eb9f593a9250f4d8750
Kevin Miller <kevin@driftabout.com>
2017-12-14 16:45:13 -0800

    tried to keep up but could not

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
 // Let's make an AJAX call to the GitHub API and then do a simple render of the data into the DOM
 
 $.ajax({
-  url: 'https://api.github.com/user/repos?type=owner',
+  url: 'https://api.github.com/users/driftabout',
   method: 'GET',
   headers: {
-    Authorization: 'token copy-your-token-here'
+    Authorization: '5c956eb3c858ec192ee2812b86c31b7cb2e5b1e9'
   }
 })
   .then(
--- server.js
+++ server.js
@@ -2,12 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 const cors = require('cors');
+const superagent = require('superagent')
 
 app.use(express.static('./public'));
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.sendFile('index.html')
+});
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html')
+});
+
+app.get('/github/*', (req, res) => {
+  const url = `https://api.github.com/${req.params[0]}`
+  superagent.get(url)
+  .set(`Authorization`, `token ${process.env.TOKEN}`)
+  .then{(repos) => res.send(repos), (err) => res.send(err)}
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
